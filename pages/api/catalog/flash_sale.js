82cf09cc8a01f00fd480f8472e91cf2e85a72d1e
Kameel <ahmaadm84112@gmail.com>
2022-11-05 03:41:56 -0700

    testing deploy `[2]`

--- pages/api/catalog/flash_sale.js
+++ pages/api/catalog/flash_sale.js
@@ -1,9 +1,8 @@
 require('dotenv').config()
-const mysql = require('mysql2')
 // db setup ✨
-const conn = mysql.createConnection(process.env.DATABASE_URL)
-
 export default function handler( req, res) {
+	const mysql = require('mysql2')
+	const conn = mysql.createConnection(process.env.DATABASE_URL)
 	if(req.method == "GET"){
 		conn.query("SELECT * FROM produk WHERE flash_sale ='YES'", (error, result) => {
 			if(error){
--- pages/api/catalog/id/[specificProduct].js
+++ pages/api/catalog/id/[specificProduct].js
@@ -1,7 +1,8 @@
-import conn from '../../../../db'
- import { useRouter } from 'next/router'
-
+require('dotenv').config()
+// db setup ✨
 export default function handler( req, res) {
+	const mysql = require('mysql2')
+	const conn = mysql.createConnection(process.env.DATABASE_URL)
 	const id = req.query.specificProduct
 	if(req.method == "GET"){
 		conn.query(`SELECT * FROM produk WHERE id_produk = ${id}`, (error, result) => {
